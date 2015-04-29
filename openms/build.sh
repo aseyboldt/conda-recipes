9f4081e27317ef55740a64452128f8a64ea96d3f
Adrian Seyboldt <adrian_seyboldt@web.de>
2015-04-29 17:48:20 +0200

    build libsvm in openms contrib

--- openms/build.sh
+++ openms/build.sh
@@ -9,16 +9,17 @@ git clone https://github.com/OpenMS/contrib
 cd contrib
 git checkout f6b44d770b6c1b23972a8891753472e60a356253
 
-cmake . -DBUILD_TYPE=BOOST
+#cmake . -DBUILD_TYPE=BOOST
 cmake . -DBUILD_TYPE=SEQAN
 cmake . -DBUILD_TYPE=WILDMAGIC
 cmake . -DBUILD_TYPE=EIGEN
+cmake . -DBUILD_TYPE=LIBSVN
 
 echo "Build of contrib is complete"
 
 cd ${SRC_DIR}
 
-cmake . -DCMAKE_PREFIX_PATH=../contrib -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}
+cmake . -DCMAKE_PREFIX_PATH=../contrib -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} -DBOOST_USE_STATIC=OFF -DHAS_XSERVER=off
 
 if [ ${CPU_COUNT} -lt 20 ]
 then
@@ -27,6 +28,7 @@ else
 	PROC_COUNT=20
 fi
 
-make -j ${PROC_COUNT}
+make -j ${PROC_COUNT} OpenMS
+make -j ${PROC_COUNT} TOPP
 make install
 
